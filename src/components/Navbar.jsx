71b87ab8c1d915a07487fa2c3f8ebf443352cd7c
João Valente <joao.valente5935@gmail.com>
2023-11-06 16:28:13 +0000

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,14 @@
-function Navbar() {}
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return(
+        <nav>
+            <h1>WikiCountrie</h1>
+            <Link to="/">Home</Link>
+            <Link to="/:countryId">Country</Link>
+        </nav>
+    )
+}
 
 export default Navbar;
